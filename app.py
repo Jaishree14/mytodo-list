c4c8b167ab46404a20d4e9949a91508cb3f49205
Jaishree14 <60139315+Jaishree14@users.noreply.github.com>
2021-09-08 15:32:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import (
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,6 +14,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.secret_key = 'my_precious'
+
+
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
@@ -15,8 +26,20 @@ class Todo(db.Model):
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
+    
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
-@app.route('/', methods=['GET', 'POST'])
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='admin', password='admin'))
+
+@app.route('/todolist', methods=['GET', 'POST'])
 def hello_world():
     if request.method=='POST':
         title = request.form['title']
@@ -32,7 +55,7 @@ def hello_world():
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return 'this is products page'
+   
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
@@ -44,7 +67,7 @@ def update(sno):
         todo.desc = desc
         db.session.add(todo)
         db.session.commit()
-        return redirect("/")
+        return redirect("/todolist")
         
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
@@ -54,7 +77,32 @@ def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect("/")
+    return redirect("/todolist")
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('hello_world'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
